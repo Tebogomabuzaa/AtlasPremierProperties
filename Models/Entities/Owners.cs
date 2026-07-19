9f4e4e671bc385845a7a48a506ed2d064d643f5c
Tebogo Jr Mabuza <tebogomabuzaaa@gmail.com>
2026-07-19 16:24:31 +0200

    Added a class for owners table

---
+++ Models/Entities/Owners.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AtlasPremierProperties.Models.Entities
+{
+    public class Owners
+    {
+        // Add comments
+        public int OwnerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string FullName { get { return FirstName + " " + LastName; } }
+    }
+
+}
