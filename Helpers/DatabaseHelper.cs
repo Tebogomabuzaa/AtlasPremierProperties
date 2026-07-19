f506080305f14ad25d2d003444f547d1a8d22e7c
Tebogo Jr Mabuza <tebogomabuzaaa@gmail.com>
2026-07-19 16:20:22 +0200

    Created database connection

---
+++ Helpers/DatabaseHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+// Add comments
+using System.Configuration;
+using System.Data.OleDb;
+
+namespace AtlasPremierProperties.Helpers
+{
+    public class DatabaseHelper
+    {
+        private readonly string _connectionString;
+
+        public DatabaseHelper()
+        {
+            string dbPath = ConfigurationManager.AppSettings["DatabasePath"];
+
+            _connectionString =
+                "Provider=Microsoft.ACE.OLEDB.12.0;" +
+                "Data Source=" + dbPath + ";" +
+                "Persist Security Info=False;";
+        }
+
+        public OleDbConnection GetConnection()
+        {
+            return new OleDbConnection(_connectionString);
+        }
+    }
+}
