7fdc077aac8fca6320253d2456fa2409bf7b632b
Tebogo Jr Mabuza <tebogomabuzaaa@gmail.com>
2026-07-19 16:32:57 +0200

    added class for property entity

---
+++ Models/Entities/Property.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AtlasPremierProperties.Models.Entities
+{
+    // Add comment
+    public class Property
+    {
+        public int PropertyID { get; set; }
+        public string PropertyDescription { get; set; }
+        public string PhysicalAddress { get; set; }
+        public int OwnerID { get; set; }
+        public decimal MonthlyRent { get; set; }
+        public bool VacancyStatus { get; set; }
+
+        // Not a database column - populated via JOIN
+        public string OwnerName { get; set; }
+    }
+}
