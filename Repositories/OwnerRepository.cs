e49d1c283cf7c83cc0d2bae1f84f35eb13c378a7
MbaliMabombo <mbalimabombo@gmail.com>
2026-08-30 22:13:29 +0200

    OwnerRepository created

---
+++ Repositories/OwnerRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDB;
+using System.Runtime.InteropServices.ComTypes;
+using AtlasPremierProperties.Helpers;
+using AtlasPremierProperties.Models.Entities;
+
+
+namespace AtlasPremierProperties.Repositories
+{
+	public class OwnerRepository
+	{
+		private readonly DatabaseHelper.db;
+
+		public OwnerRepository()
+		{
+			_db = new DatabaseHelper():
+		}
+	}
+}
+
